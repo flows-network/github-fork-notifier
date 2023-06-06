b875b9497aacb37cf86a54ce196901bb0853d698
Ji Chen <jaykchen@gmail.com>
2023-06-06 16:12:33 -0400

    org may be null, test data.json

--- src/lib.rs
+++ src/lib.rs
@@ -42,43 +42,26 @@ async fn handler(payload: EventPayload) {
         let mut email = "".to_string();
         let mut twitter_handle = "".to_string();
 
-        // let user_route = format!("https://api.github.com/users/jaykchen");
         let user_route = format!("users/{forkee_login}");
         let response: OctoResult<User> = octocrab.get(&user_route, None::<&()>).await;
         match response {
-            Err(_) => {
-                send_message_to_channel(
-                    &slack_workspace,
-                    &slack_channel,
-                    "error getting email".to_string(),
-                );
-            }
+            Err(_) => {}
             Ok(user_obj) => {
                 email = user_obj.email.unwrap_or("".to_string());
                 twitter_handle = user_obj.twitter_username.unwrap_or("".to_string());
-                send_message_to_channel(&slack_workspace, &slack_channel, email.clone());
             }
         }
 
         let mut org_name = "".to_string();
         let mut org_company = "".to_string();
 
-        // let org_route = format!("https://api.github.com/orgs/flows-network");
         let org_route = format!("orgs/{forkee_login}");
         let response: OctoResult<Organization> = octocrab.get(&org_route, None::<&()>).await;
         match response {
-            Err(_) => {
-                send_message_to_channel(
-                    &slack_workspace,
-                    &slack_channel,
-                    "error getting org".to_string(),
-                );
-            }
+            Err(_) => {}
             Ok(org_obj) => {
                 org_name = org_obj.name.unwrap_or("no org name".to_string());
                 org_company = org_obj.company.unwrap_or("no company name".to_string());
-                send_message_to_channel(&slack_workspace, &slack_channel, org_name.clone());
-
             }
         };
 
@@ -95,6 +78,8 @@ async fn handler(payload: EventPayload) {
         "Org": org_url,
         "Created": time,
         });
+        send_message_to_channel(&slack_workspace, &slack_channel, data.to_string());
+ 
         create_record(
             &airtable_token_name,
             &airtable_base_id,
